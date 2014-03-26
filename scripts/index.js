eff5f9b5e4e483f04703897dead5fae27e624c26
Breno L. Freitas <brenolimadefreitas@gmail.com>
2014-03-25 22:18:58 -0400

    Interfaces changes

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
             target: e,
             paintStyle: {
                 lineWidth: 3,
-                strokeStyle: "red"
+                strokeStyle: "#999"
             },
             connector: ["Straight"]
         });

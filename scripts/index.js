de8ddb4b13e423a0c6a105615854754834224241
Breno L. Freitas <brenolimadefreitas@gmail.com>
2014-03-26 16:19:25 -0400

    Added dynamic content from sample.json.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,47 @@
+window.threshold = 200;
+
 $(document).ready(function() {
 
-    console.log("hey");
+    var block = function(author, content, id) {
+        var dots = content.length > window.threshold ? ' ...' : '';
+
+        return $('<div class="block" id="' + id + '">\
+            <div class="author">' + author + ' says:</div>\
+            <div class="txt">\
+            ' + content.substring(0, window.threshold) + dots + '\
+            </div>\
+            <div class="bar">\
+                <span class="btn">\
+                    <i class="fa fa-thumbs-up fa-2x"></i>\
+                </span>\
+                <span class="btn">\
+                    <i class="fa fa-thumbs-down fa-2x"></i>\
+                </span>\
+                <span class="btn r">\
+                    <i class="fa fa-mail-reply fa-2x"></i>\
+                </span>\
+                <span class="clear"></span>\
+            </div>\
+        </div>');
+    };
+
+    var hardcoded = parseInt(window.location.hash.substring(1)) || 9;
+
+    var prt = data[hardcoded];
+
+    $(".parent").append(block(prt.author, prt.content, "prt"));
+
+    for (var i = 0, c = 0; i < data.length && c < 3; i++) {
+        var e = data[i];
+
+        if (e.ID != prt.ID && e.parents.indexOf(prt.ID) != -1) {
+            $(".children").append(block(e.author, e.content, "child" + c));
+            c++;
+        }
+    }
 
     $(".children .block").each(function () {
-        var e0 = jsPlumb.addEndpoint("p", {
+        var e0 = jsPlumb.addEndpoint("prt", {
             anchor: "BottomCenter",
             endpoint: "Blank"
         });
@@ -20,7 +58,9 @@ $(document).ready(function() {
                 lineWidth: 3,
                 strokeStyle: "#999"
             },
-            connector: ["Straight"]
+            connector: "StateMachine",
+            endpoint: "Blank",
+            overlays:[ ["PlainArrow", {location:100, width:20, length:10} ]]
         });
     });
 

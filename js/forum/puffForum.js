732c52908582b831ede07522f6a009573831aada
Mattasher <github@quo.org>
2014-04-04 14:17:44 -0400

    Restructuring

---
+++ js/forum/puffForum.js
@@ -0,0 +1,14 @@
+/*
+ addPost(content) #Takes content field from the form as a string, returns an ID
+ - Looks for the users name is the system.
+
+ # Pulls info from localStorage and from network into a graph db stored client side
+ # By calling puff.js and asking for things.
+ puffGraph
+ .edges
+ .nodes
+
+ onNewContent()
+
+ getRootPuffs(N) # Goes into puffGraph, gets out N most recent root puffs
+ */

c1d65d4104cc3317e61748e7ae1f33bd218e3a10
Crzy Rbit <crzyrbit@users.noreply.github.com>
2014-04-05 17:15:39 -0400

    Put main Puff HTML & header into partials.
    Put main Puff HTML into partials folder.
    Make the Zoom controls a partial to the main page.

---
+++ angular/app/js/nginclude-templates.js
@@ -0,0 +1,6 @@
+function includesController($scope) {
+  // $scope.templates =
+  //   [ { name: 'zoom-controlls.html', url: 'zoom-controlls.html'},
+  //     { name: 'partials/test.html', url: 'partials/test.html'} ];
+  $scope.template = {url: 'partials/subs/zoom-controlls.html'};
+}
--- angular/app/lib/zoom.js
+++ angular/app/lib/zoom.js
@@ -31,8 +31,9 @@ $(function() {
       max: 48,
       slide: function( event, ui ) {
         // Updated text field with font size. 
-        // TODO: this isn't on the main spec here http://extrazoom.com/image-10847.html, delete or keep?
+        // TODO: Showing the sliders value isn't on the main spec here http://extrazoom.com/image-10847.html, delete or keep?
         $( "#text-zoom" ).val( ui.value );
+        console.log( "Text Size: " + $( "#slider-puff-text-zoom" ).slider( "value" ) );
 
         // When slider is changed updated font-size in main Puff block text.
         $(".block").css( { "font-size": ui.value } );
@@ -45,7 +46,6 @@ $(function() {
     // can get rid of it if we aren't keeping this part of the GUI, see TODO above.
     // It may be usefull for debugging until the GUI is more finalized.
     $( "#text-zoom" ).val( $( "#slider-puff-text-zoom" ).slider( "value" ) );
-
 
     //
     //  Slider for Puff/Block Branching level.

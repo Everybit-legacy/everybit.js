557e0e46089eee30eb6b9ac144aaaacbd13d8d6f
dann toliver <dann@bentobox.net>
2014-04-09 13:08:12 -0400

    strides toward sigs

--- js/forum/puffForum.js
+++ js/forum/puffForum.js
@@ -1,4 +1,4 @@
-/*
+/* 
                    _____  _____  _____                           
     ______  __ ___/ ____\/ ____\/ ____\___________ __ __  _____  
     \____ \|  |  \   __\\   __\\   __\/  _ \_  __ \  |  \/     \ 
@@ -19,6 +19,7 @@ PuffForum = {}
 
 PuffForum.graph = {}
 PuffForum.newPuffCallbacks = []
+PuffForum.userinfo = {}
 
 PuffForum.init = function() {
   //// set up everything. 
@@ -78,14 +79,54 @@ PuffForum.getRootPuffs = function(limit) {
 
 PuffForum.addPost = function(content, parents) {
   //// Given a string of content, create a puff and push it into the system
-
-  // if there's no user, add an anonymous one
- 
+  
   // scrub parents -- if they're puffs extract ids, then ensure parents is an array
+  if(!Array.isArray(parents))
+    parents = [parents]
+    
+  if(parents.map(PuffForum.getPuffById).filter(function(x) { return x != null }).length != parents.length)
+    return "Error Error Error: those are not good parents"
  
-  // var sig = Puff.createPuff(user, privkey, 'text', content, {time: Date.now(), parents: parents})
+  // if there's no user, add an anonymous one
+  // THINK: where should the username/privatekey live? we'll put it here for now, but some other layer should take responsibility.
+  if(!PuffForum.userinfo.username || !PuffForum.userinfo.privateKey)
+    return PuffForum.addAnonUser(function(username) {PuffForum.addPost(content, parents)})
+
+  var sig = Puff.createPuff(PuffForum.userinfo.username, PuffForum.userinfo.privateKey, 'text', content, {time: Date.now(), parents: parents})
  
+  // THINK: actually we can't return this because we might go async
   // return sig;
+}
+
+
+PuffForum.addAnonUser = function(callback) {
+  //// statefully state a new user and register it and store it and oh dear
+  
+  // gen privkey
+  var privateKey = Puff.generatePrivateKey()
+  
+  // gen pubkey
+  var publicKey = ""
+  
+  PuffForum.userinfo.privateKey = privateKey
+  PuffForum.userinfo.publicKey  = publicKey
+  
+  $.ajax({
+    type: 'POST',
+    url: 'api.php',
+    data: {
+      type: 'addUser',
+      publicKey: PuffForum.userinfo.publicKey
+    },
+    success:function(result){
+      PuffForum.userinfo.username = result.username
+      callback(result.username)
+    },
+    error: function() {
+      console.log('Error Error Error: the anonymous user could not be added')
+    },
+    dataType: 'json'
+  });
 }
 
 PuffForum.onNewPuffs = function(callback) {
--- js/puffballPlatform/puffball.js
+++ js/puffballPlatform/puffball.js
@@ -84,6 +84,31 @@ Puff.createPuff = function(username, privatekey, type, content, meta) {
   return puff
 }
 
+Puff.generatePrivateKey = function() {
+  //// from http://procbits.com/2013/08/27/generating-a-bitcoin-address-with-javascript
+  
+  var randArr = new Uint8Array(32) //create a typed array of 32 bytes (256 bits)
+  window.crypto.getRandomValues(randArr) //populate array with cryptographically secure random numbers
+  
+  var privateKeyBytes = []  //some Bitcoin and Crypto methods don't like Uint8Array for input. They expect regular JS arrays.
+  for (var i = 0; i < randArr.length; ++i)
+    privateKeyBytes[i] = randArr[i]
+  
+  return privateKeyBytes
+  // var privateKeyWIF = new Bitcoin.Address(privateKeyBytes) 
+  // privateKeyWIF.version = 0x80 //0x80 = 128, https://en.bitcoin.it/wiki/List_of_address_prefixes
+  // return privateKeyWIF.toString()  
+}
+
+Puff.privateToPublic = function(privateKeyBytes) {
+  //// from http://procbits.com/2013/08/27/generating-a-bitcoin-address-with-javascript
+  
+  //privateKeyBytes is the private key array from the top
+  var eckey = new Bitcoin.ECKey(privateKeyBytes) 
+  eckey.compressed = true
+  var publicKeyHex = Crypto.util.bytesToHex(eckey.getPub())
+  return publicKeyHex
+}
 
 Puff.signPayload = function(payload, privatekey) {
   //// sign the hash of a payload with a private key and return the sig
@@ -98,6 +123,11 @@ Puff.signPayload = function(payload, privatekey) {
   
   /*
   
+  - generate privkey, store in local as 256bit int in hex (or as a base58 WIF)
+  - generate compressed pubkey, store in local as 33 bytes of hex (or base58)
+  - send pubkey to server, get username
+  - 
+  
       x = new Bitcoin.ECKey('bananas')
         Bitcoin.ECKey.r {priv: BigInteger, compressed: false, setCompressed: function, getPub: function, getPubPoint: function…}
       sig = sign_message(x, 'foo123 ok hi')
@@ -107,6 +137,69 @@ Puff.signPayload = function(payload, privatekey) {
       x.getBitcoinAddress().toString()
         "1LDShdXPqw85q5FmkuXdBmZm3bMfw423uw"
   
+var curve = getSECCurveByName("secp256k1") //found in bitcoinjs-lib/src/jsbn/sec.js
+
+//convert our random array or private key to a Big Integer
+var privateKeyBN = BigInteger.fromByteArrayUnsigned(input) 
+
+var curvePt = curve.getG().multiply(privateKeyBN)
+var x = curvePt.getX().toBigInteger()
+var y = curvePt.getY().toBigInteger()
+var publicKeyBytes = integerToBytes(x,32) //integerToBytes is found in bitcoinjs-lib/src/ecdsa.js
+publicKeyBytes = publicKeyBytes.concat(integerToBytes(y,32))
+publicKeyBytes.unshift(0x04)
+var publicKeyHex = Crypto.util.bytesToHex(publicKeyBytes)
+
+console.log(publicKeyHex)
+  
+  
+  
+  
+var randArr = new Uint8Array(32) //create a typed array of 32 bytes (256 bits)
+window.crypto.getRandomValues(randArr) //populate array with cryptographically secure random numbers
+
+[129, 9, 142, 182, 195, 40, 51, 173, 166, 150, 242, 111, 180, 170, 80, 63, 191, 176, 216, 68, 236, 149, 166, 100, 175, 180, 5, 93, 176, 247, 106, 47]
+var privateKeyBytes = []
+for (var i = 0; i < randArr.length; ++i)
+  privateKeyBytes[i] = randArr[i]
+47
+privateKeyBytes
+[129, 9, 142, 182, 195, 40, 51, 173, 166, 150, 242, 111, 180, 170, 80, 63, 191, 176, 216, 68, 236, 149, 166, 100, 175, 180, 5, 93, 176, 247, 106, 47]
+var privateKeyHex = Crypto.util.bytesToHex(privateKeyBytes).toUpperCase()
+console.log(privateKeyHex) //1184CD2CDD640CA42CFC3A091C51D549B2F016D454B2774019C2B2D2E08529FD
+81098EB6C32833ADA696F26FB4AA503FBFB0D844EC95A664AFB4055DB0F76A2F VM5968:3
+undefined
+var privateKeyHex = Crypto.util.bytesToHex(privateKeyBytes).toUpperCase()
+undefined
+privateKeyHex
+"81098EB6C32833ADA696F26FB4AA503FBFB0D844EC95A664AFB4055DB0F76A2F"
+privateKeyHex = Crypto.util.bytesToHex(privateKeyBytes).toUpperCase()
+"81098EB6C32833ADA696F26FB4AA503FBFB0D844EC95A664AFB4055DB0F76A2F"
+var privateKeyWIF = new Bitcoin.Address(privateKeyBytes) 
+undefined
+privateKeyWIF = new Bitcoin.Address(privateKeyBytes) 
+Bitcoin.Address {hash: Array[32], version: 0, toString: function, getHashBase64: function}
+privateKeyWIF.version = 0x80
+128
+privateKeyWIF = privateKeyWIF.toString()
+"5Jo7fe6hD4HfVJRgHuRRy7prRUpP1x2LmM4wXVMf3FLVXJKme4F"
+  
+  
+var curve = getSECCurveByName("secp256k1") //found in bitcoinjs-lib/src/jsbn/sec.js
+
+//convert our random array or private key to a Big Integer
+var privateKeyBN = BigInteger.fromByteArrayUnsigned(privateKeyWIF) 
+
+var curvePt = curve.getG().multiply(privateKeyBN)
+var x = curvePt.getX().toBigInteger()
+var y = curvePt.getY().toBigInteger()
+var publicKeyBytes = integerToBytes(x,32) //integerToBytes is found in bitcoinjs-lib/src/ecdsa.js
+publicKeyBytes = publicKeyBytes.concat(integerToBytes(y,32))
+publicKeyBytes.unshift(0x04)
+var publicKeyHex = Crypto.util.bytesToHex(publicKeyBytes)
+
+console.log(publicKeyHex)
+04b9961810aa99c96413840ac443e8929c95ac462e713e0d2abfe3f20a3351cc39ca75c7818341e679b610876feb320b7a294ddd0bd399aeb0e7e339e80c502833 
   */
 }
 

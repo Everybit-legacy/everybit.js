89f6ab878f41fc2cc536e3c8e8b03ac6c5fbc0fe
dann toliver <dann@bentobox.net>
2014-04-11 13:38:06 -0400

    some stub functions

--- js/freeBeer/main.js
+++ js/freeBeer/main.js
@@ -3,13 +3,13 @@
 /*
 
   Demo todos:
-  - P2P on FB
+  -- P2P on FB
   - alternate server + p2p
   - fancy graphics
   - chess posts
   - parent styling
   - tree styling
-
+  - tree map
 */
 
 
@@ -49,6 +49,18 @@ PuffForum.onNewPuffs(eatPuffs);
 PuffForum.init();
 
 ////////// End PuffForum Interface ////////////
+
+
+
+/////// minimap ////////
+
+var updateMinimap = function() {  
+  var mapdom = $('#minimap')
+  mapdom.append(JSON.stringify(Puff.Data.puffs))
+}
+
+
+////// end minimap /////
 
 
 
@@ -174,6 +186,17 @@ showPuff = function(puff) {
     });
 }
 
+
+// set state to current puff
+function setBrowserHistoryPuffStateToSomething(puff) {
+  var state = { 'puff': puff.sig }
+  history.pushState(state, '', '#puff=' + puff.sig)
+}
+
+// onload, store the puff id we're looking for
+function storePuffIdToSomePlaceOnLoad() {
+  someSillyGlobalPuffId = window.location.hash.substring(1)
+}
 
 
 // window.location.hash returns the anchor part of the browsers URL,

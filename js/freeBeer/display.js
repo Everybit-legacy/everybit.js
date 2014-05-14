36f9f40593ec0b06cc7d5fae0c3c92ec617781d1
dann toliver <dann@bentobox.net>
2014-05-14 16:25:26 -0400

    re-capturing errors for display in the results pane

--- js/freeBeer/display.js
+++ js/freeBeer/display.js
@@ -151,9 +151,13 @@ var PuffPacker = React.createClass({
         var pprom = PuffNet.updateUserRecord(puff)
         
         pprom.then(function(result) {
-            self.state.result = result;
-            events.pub('ui/puff-packer/userlookup', {});
-        });
+                self.state.result = result;
+                events.pub('ui/puff-packer/userlookup', {});
+             })
+             .catch(function(err) {
+                 self.state.result = {'FAIL': err.message};
+                 events.pub('ui/puff-packer/userlookup/failed', {});
+             })
     },
 
     handleShowResultsFormatted: function() {

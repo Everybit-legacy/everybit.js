cf6f606404b44ee34fdbec1aa28cd240b70818eb
Susan Sun <zhaoxisun@gmail.com>
2014-06-17 17:16:59 -0400

    adding viewraw

--- js/freeBeer/src/puffbox.js
+++ js/freeBeer/src/puffbox.js
@@ -86,8 +86,9 @@ var PuffBar = React.createClass({
 		return (
 			<div className={className}>
 				{puff.payload.type=='image' ? link : ''}
+                {puff.payload.type=='bbcode'||puff.payload.type=='markdown'||puff.payload.type=='PGN' ? <PuffViewRaw puff={puff} /> : ''}
                 <PuffFlagLink sig={puff.sig} />
-				<PuffInfoLink puff={puff} />
+                <PuffInfoLink puff={puff} />
 				<PuffChildrenCount puff={puff} />
 				<PuffParentCount puff={puff} />
 				<PuffPermaLink sig={puff.sig} />
@@ -239,6 +240,25 @@ var PuffInfoLink = React.createClass({
     }
 });
 
+var PuffViewRaw = React.createClass({
+    handleClick:function() {
+        var puff = this.props.puff;
+        showPuff(puff.sig)
+       // return puff;
+     //   return events.pub('ui/show/children', {'view.style': 'PuffAllChildren', 'view.puff': puff})
+    },
+    render: function() {
+
+        return (
+        <span className="icon">
+            <a href={'#' + this.props.sig} onClick={this.handleClick}>
+                <i className="fa fa-file-code-o"></i>
+            </a>
+        </span>
+        );
+    }
+
+})
 
 var PuffChildrenCount = React.createClass({
     handleClick: function() {

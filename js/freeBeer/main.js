21d4a011104b5d3cd66c98ffbdf35437752859ab
dann toliver <dann@bentobox.net>
2014-07-04 14:12:35 -0400

    splitting event system into its own file

--- js/freeBeer/main.js
+++ js/freeBeer/main.js
@@ -79,99 +79,6 @@ puffworlddefaults = puffworldprops                  // it's immutable so we don'
 
 
 
-///// event stuff. move this into either PuffForum or Puff itself.
-
-events = {}
-events.subs = {}
-
-events.pub = function(path, data) {
-    return setImmediate(function() {events.start_pub(path, data)})              // do it next tick
-}
-
-events.sub = function(path, handler) {
-    path = events.scrub_path(path).join('/')
-    if(!events.subs[path]) events.subs[path] = []
-    events.subs[path].push(handler)
-}
-
-events.unsub = function(path, handler) {
-    path = events.scrub_path(path).join('/')
-
-    var subs = events.subs[path]
-    if(!subs) return false
-
-    var index = subs.indexOf(handler)
-    if(index == -1) return false
-
-    subs.splice(index, 1)
-}
-
-events.start_pub = function(path, data) {
-    //// pub to * at each level and then to path itself
-    var pathlist = events.scrub_path(path)
-    var realpath = pathlist.join('/')
-
-    events.try_pub('*', data, realpath)                                         // global catchall
-
-    pathlist.reduce(function(acc, seg) {                                        // channel catchalls
-        var newacc = acc + seg + '/'
-        events.try_pub(newacc + '*', data, realpath)
-        return newacc
-    }, '')
-
-    events.try_pub(realpath, data, realpath)                                    // actual channel
-}
-
-events.try_pub = function(path, data, realpath) {
-    var handlers = events.subs[path]
-    if(!handlers || !handlers.length) return false
-    handlers.forEach(function(handler) {handler(data, realpath)})
-    // THINK: use setImmediate here?
-}
-
-// event.sub = function(path, handler) {
-//     //// add the handler to the tree
-//     path = event.scrub_path(path)
-//     var tree = event.tree
-//     var last = path.length - 1
-//     for(var i = 0; i < last; i++) {                                            // walk down, but skip the last item
-//         if(!tree[path[i]]) 
-//             tree[path[i]] = {kids: {}, handlers: []}                            // build the tree as needed
-//         tree = tree[path[i]].kids                                               // step down one level
-//     }
-//     
-//     tree[path[last]] = tree[path[last]]
-//                      ? tree[path[last]].concat(handler)                         // add handler to existing list
-//                      : [handler]                                                // or create a new one
-// }
-
-// event.unsub = function(path, handler) {
-//     // remove the handler from the tree
-//     path = event.scrub_path(path)
-//     var tree = event.tree
-//     for(var i = 0; i < path.length; i++) {
-//         tree = tree[path[i]]
-//         if(!tree) return false
-//     }
-//     
-// }
-
-
-events.scrub_path = function(path) {
-    return path.replace(/^[^\w*-]+/, '')                                        // trim leading slashes etc
-        .replace(/[^\w*-]+$/, '')                                        // trim trailing gunk
-        .split('/')                                                      // break out the path segments
-        .map(function(item) {return item.replace(/[^\w*-]/g, '')})       // scrub each segment
-}
-
-
-eventlog = []
-events.sub('*', function(data, path) {
-    eventlog.push([path, data])
-})
-
-
-
 
 //// event bindings for controlling core behavior from the display
 
@@ -648,7 +555,7 @@ PuffWardrobe.setPref('storeKeychain', true); // TODO: make this based on config,
 
 setViewPropsFromURL(); // handle pushstate hash
 
-window.addEventListener('resize', function(){
+window.addEventListener('resize', function() {
     updateUI();
 });
 

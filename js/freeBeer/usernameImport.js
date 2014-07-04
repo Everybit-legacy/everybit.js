afe789d9f646628c4afc9ab3296ae6c240d0c871
hellowsummer <nihaollw@gmail.com>
2014-07-04 17:32:58 -0400

    username import

--- js/freeBeer/usernameImport.js
+++ js/freeBeer/usernameImport.js
@@ -1,4 +1,7 @@
 var UsernameImport = {};
+UsernameImport.update(loaded, created, total) {
+	document.getElementById("importContent").innerHTML = "Start import content...<br>" + "Loaded: " + loaded + "/" + total + "<br>Created: " + created + "/" + total;
+}
 
 UsernameImport['instagram'] = {
 	'client_id': 'a62caaef5b6047d68445bb91653e585b',
@@ -8,10 +11,14 @@ UsernameImport.instagram.requestAuthentication = function() {
 	var auth_url = 'https://api.instagram.com/oauth/authorize/?client_id=' + UsernameImport.instagram.client_id + '&redirect_uri=' + UsernameImport.instagram.redirect_uri + '&response_type=code';
 	window.location = auth_url;
 };
+
 UsernameImport.instagram.importContent = function(result){
-	var allImages = [];
+	var loadedCount = 0;
+	var createdCount = 0;
 	if (result.meta.code == 200) {
 		var data = result.data;
+		total = data.length;
+		UsernameImport.update(0, 0, total);
 		for (var i=0; i<data.length; i++) {
 			var entry = data[i];
 			if (entry.type != 'image') continue;
@@ -25,44 +32,55 @@ UsernameImport.instagram.importContent = function(result){
 			img_el.setAttribute('src', src);
 			img_el.setAttribute('width', width);
 			img_el.setAttribute('height', height);
-
-			img_el.onload = function() {
-				// getBase64Image
-			    // Create an empty canvas element
+			img_el.onload = function(){
+				loadedCount++;
+				UsernameImport.update(loadedCount, createdCount, total);
+				var img_el = this;
 			    var canvas = document.createElement("canvas");
-			    canvas.height = width;
-			    canvas.width = height;
+			    canvas.height = img_el.width;
+			    canvas.width = img_el.height;
 
 			    var ctx = canvas.getContext("2d");
-			    var img = "";
-			    var img_el = this;
-		    	ctx.drawImage(img_el, 0, 0);
-				img = canvas.toDataURL('image/jpeg');
+				ctx.drawImage(img_el, 0, 0);
+				var img = canvas.toDataURL('image/jpeg');
 
 				var metadata = {
 					time: entry.created_time * 1000,
 					tags: entry.tags,
 					caption: entry.caption.text
 				}
-
-				allImages.push({img: img, metadata:metadata});
-				
 				var post_prom = PuffForum.addPost('image', img, [], metadata);
 				post_prom.then(function(puff){
+					createdCount++;
+					UsernameImport.update(loadedCount, createdCount, total);
 					console.log(puff.sig);
+					published.push(puff.sig);
 				}).catch(function(err){
 					console.log(err.message);
 				})
-			}
+			};
 		}
+
+		var clear = window.setTimeout(function(){
+			var script = document.getElementById('instagramContent');
+			script.parentElement.removeChild(script);
+			console.log('clear');
+		}, data.length * 100);
+		return true;
+	} else {
+		document.getElementById("importContent").innerHTML = "Trouble getting content."
 	}
+	return false;
 }
 UsernameImport.instagram.contentURL = function(username, userid, access_token) {
 	var content_url = "https://api.instagram.com/v1/users/" + userid + "/media/recent/?access_token=" + access_token + "&count=100&callback=UsernameImport.instagram.importContent";
-	var newScript_el = document.createElement('script');
-	newScript_el.setAttribute("src", content_url);
-	newScript_el.setAttribute("id", "instagramContent");
-	document.getElementsByTagName('head')[0].appendChild(newScript_el);
+	if (PuffWardrobe.switchCurrent(username)) {
+		var newScript_el = document.createElement('script');
+		newScript_el.setAttribute("src", content_url);
+		newScript_el.setAttribute("id", "instagramContent");
+		return document.getElementsByTagName('head')[0].appendChild(newScript_el);
+	}
+	return false;
 }
 
 

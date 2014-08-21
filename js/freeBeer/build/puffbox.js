3d15e6f7a1f44a8f7f3b090045a54ebedf412cca
Susan Sun <zhaoxisun@gmail.com>
2014-08-21 11:25:50 -0400

    fix for extra filter in viewing parent/children inside show my puffs/puffs for me

--- js/freeBeer/build/puffbox.js
+++ js/freeBeer/build/puffbox.js
@@ -342,7 +342,8 @@ var PuffParentCount = React.createClass({displayName: 'PuffParentCount',
     handleClick: function() {
         var puff  = this.props.puff;
         return events.pub('ui/show/parents', { 'view.mode': 'list'
-                                             , 'view.query': puffworlddefaults.view.query
+                                             ,'view.filters': {}
+                                             ,'view.query': puffworlddefaults.view.query
                                              , 'view.query.focus': puff.sig
                                              , 'view.query.ancestors': 1
                                              })
@@ -577,6 +578,7 @@ var PuffChildrenCount = React.createClass({displayName: 'PuffChildrenCount',
     handleClick: function() {
         var puff  = this.props.puff;
         return events.pub('ui/show/parents', { 'view.mode': 'list'
+                                             ,'view.filters': {}
                                              , 'view.query': puffworlddefaults.view.query
                                              , 'view.query.focus': puff.sig
                                              , 'view.query.descendants': 1

c9589e7459206009117aae13e38e4f4c1e49500d
arafeek <adam_rafeek@hotmail.com>
2014-12-11 12:18:40 -0500

    bumped to version 0.7.3

--- core/PB.js
+++ core/PB.js
@@ -26,7 +26,7 @@ if(!PB.CONFIG) PB.CONFIG = {}                           // or we might not
 PB.Modules = {}                                         // supplementary extensions live here
 PB.M = PB.Modules
 
-PB.version = '0.7.2'
+PB.version = '0.7.3'
 
 ////////////// STANDARD API FUNCTIONS //////////////////
 

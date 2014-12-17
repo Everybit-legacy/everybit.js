2b4d3599ed524f29862d9e920f64e873f0a10c7d
Mattasher <github@quo.org>
2014-12-17 09:03:34 -0500

    Forum changes

--- modules/PB.M.Forum.js
+++ modules/PB.M.Forum.js
@@ -1,19 +1,19 @@
 /* 
-                   _____  _____  _____                           
-    ______  __ ___/ ____\/ ____\/ ____\___________ __ __  _____  
-    \____ \|  |  \   __\\   __\\   __\/  _ \_  __ \  |  \/     \ 
-    |  |_> >  |  /|  |   |  |   |  | (  <_> )  | \/  |  /  Y Y  \
-    |   __/|____/ |__|   |__|   |__|  \____/|__|  |____/|__|_|  /
-    |__|                                                      \/ 
-  
-  
-  A Puffball module for managing forum-style puffs. Wraps the core Puffball API in a fluffy layer of syntactic spun sugar.
+ _____  _____  _____
+ ______  __ ___/ ____\/ ____\/ ____\___________ __ __  _____
+ \____ \|  |  \   __\\   __\\   __\/  _ \_  __ \  |  \/     \
+ |  |_> >  |  /|  |   |  |   |  | (  <_> )  | \/  |  /  Y Y  \
+ |   __/|____/ |__|   |__|   |__|  \____/|__|  |____/|__|_|  /
+ |__|                                                      \/
 
-  Usage example:
-  PB.M.Forum.init()
-  ...
 
-*/
+ A Puffball module for managing forum-style puffs. Wraps the core Puffball API in a fluffy layer of syntactic spun sugar.
+
+ Usage example:
+ PB.M.Forum.init()
+ ...
+
+ */
 
 PB.M.Forum = {};
 
@@ -52,7 +52,7 @@ PB.M.Forum.addTimestamp = function(payload) {
 PB.M.Forum.filterByFilters = function(filters) {
 
     if(!filters) return function() {return true}
-    
+
     //// get a filtering function
     return function(shell) {
 
@@ -135,9 +135,9 @@ PB.M.Forum.sortByPayload = function(a,b) {
  */
 PB.M.Forum.getParentCount = function(puff, props) {
     if(!puff) return 0
-    
+
     var sig = puff.sig || puff
-    
+
     return PB.Data.graph.v(sig).out('parent').run().length
 }
 
@@ -149,9 +149,9 @@ PB.M.Forum.getParentCount = function(puff, props) {
  */
 PB.M.Forum.getChildCount = function(puff) {
     if(!puff) return 0
-    
+
     var sig = puff.sig || puff
-    
+
     return PB.Data.graph.v(sig).out('child').run().length
 }
 
@@ -173,14 +173,14 @@ PB.M.Forum.getPuffList = function(query, filters, limit) {
 
     // var shells = PB.M.Forum.getShells(query, filters)
     var shells = PB.Data.getAllMyShells()
-    
+
     var filtered_shells = shells.filter(PB.M.Forum.filterByFilters(Boron.extend({}, query, filters)))
-                                .sort(PB.M.Forum.sortByPayload) // TODO: sort by query
+        .sort(PB.M.Forum.sortByPayload) // TODO: sort by query
 
     var sliced_shells = filtered_shells.slice(offset, offset+limit)
-    
+
     var puffs = sliced_shells.map(PB.Data.getPuffFromShell)
-                             .filter(Boolean)
+        .filter(Boolean)
 
     var have = sliced_shells.length
     // var have = puffs.length
@@ -188,7 +188,7 @@ PB.M.Forum.getPuffList = function(query, filters, limit) {
         return puffs  // as long as we have enough filtered shells the puffs will eventually fill in empty spots
 
     PB.Data.fillSomeSlotsPlease(limit, have, query, filters)
-    
+
     return puffs;
 }
 
@@ -205,15 +205,15 @@ PB.M.Forum.getPuffList = function(query, filters, limit) {
  */
 PB.M.Forum.addPost = function(type, content, parents, metadata, userRecordsForWhomToEncrypt, privateEnvelopeAlias) {
     //// Given a string of content, create a puff and push it into the system
-    
+
     // ensure parents is an array
     if(!parents) parents = []
     if(!Array.isArray(parents)) parents = [parents]
-    
+
     // ensure parents contains only puff ids
     if(parents.map(PB.getPuffBySig).filter(function(x) { return x != null }).length != parents.length)
         return PB.emptyPromise('Those are not good parents')
-    
+
     // ensure parents are unique
     parents = PB.uniquify(parents)
 
@@ -225,21 +225,21 @@ PB.M.Forum.addPost = function(type, content, parents, metadata, userRecordsForWh
         routes = metadata.routes // THINK: this should probably merge with above instead of replacing it...
         delete metadata['routes']
     }
-    
+
     // ensure all routes are unique
     routes = PB.uniquify(routes)
-    
+
     var takeUserMakePuff = PB.M.Forum.partiallyApplyPuffMaker(type, content, parents, metadata, routes, userRecordsForWhomToEncrypt, privateEnvelopeAlias)
-    
+
     // get a user promise
     var userprom = PB.Users.getUpToDateUserAtAnyCost()
-    
+
     var prom = userprom.catch(PB.catchError('Failed to add post: could not access or create a valid user'))
-                       .then(takeUserMakePuff)
-                       .catch(PB.catchError('Posting failed'))
-    
+        .then(takeUserMakePuff)
+        .catch(PB.catchError('Posting failed'))
+
     return prom
-    
+
     // NOTE: any puff that has 'time' and 'parents' fields fulfills the forum interface
     // TODO: make an official interface fulfillment thing
 }
@@ -258,9 +258,9 @@ PB.M.Forum.addPost = function(type, content, parents, metadata, userRecordsForWh
  */
 PB.M.Forum.partiallyApplyPuffMaker = function(type, content, parents, metadata, routes, userRecordsForWhomToEncrypt, privateEnvelopeAlias) {
     //// Make a puff... except the parts that require a user
-    
+
     // THINK: if you use the same metadata object for multiple puffs your cached version of the older puffs will get messed up
-    
+
     var payload = metadata || {}                            // metadata becomes the basis of payload
     payload.parents = payload.parents || parents            // ids of the parent puffs
     payload.time = metadata.time || Date.now()              // time is always a unix timestamp
@@ -269,7 +269,7 @@ PB.M.Forum.partiallyApplyPuffMaker = function(type, content, parents, metadata, 
     var type  = type || 'text'
     var routes = routes ? routes : [];
     routes = routes.concat(PB.CONFIG.zone);
-    
+
     return function(userRecord) {
         // userRecord is always an up-to-date record from the DHT, so we can use its 'latest' value here 
 
@@ -293,7 +293,7 @@ PB.M.Forum.addFamilialEdgesForShell = function(child) {
 
 PB.M.Forum.addFamilialEdgesForParent = function(child) {
     var existingParents = PB.Data.graph.v(child.sig).out('parent').property('shell').run().map(PB.prop('sig'))
-    
+
     return function(parentSig) {
         if(~existingParents.indexOf(parentSig)) return false                       // done?
         PB.Data.addSigAsVertex(parentSig)                                          // idempotent
@@ -314,7 +314,7 @@ PB.M.Forum.addFamilialEdgesForParent = function(child) {
  */
 PB.M.Forum.processContent = function(type, content, puff) {
     var typeObj = PB.M.Forum.contentTypes[type]
-    
+
     if(!typeObj)
         typeObj = PB.M.Forum.contentTypes['text']
 
@@ -338,10 +338,10 @@ PB.M.Forum.getProcessedPuffContent = function(puff) {
     // THINK: we've already ensured these are proper puffs, so we don't have to check for payload... right?
     if(PB.M.Forum.puffContentStash[puff.sig])
         return PB.M.Forum.puffContentStash[puff.sig]
-    
+
     var content = PB.M.Forum.processContent(puff.payload.type, puff.payload.content, puff)
     PB.M.Forum.puffContentStash[puff.sig] = content
-    
+
     return content
 }
 
@@ -352,14 +352,14 @@ PB.M.Forum.getProcessedPuffContent = function(puff) {
  */
 PB.M.Forum.addContentType = function(name, type) {
     // THINK: move this down into PB?
-    
-    if(!name) 
+
+    if(!name)
         return PB.onError('Invalid content type name')
     if(PB.CONFIG.supportedContentTypes && PB.CONFIG.supportedContentTypes.indexOf(name) == -1)
         return PB.onError('Unsupported content type: ' + name)
-    if(!type.toHtml) 
+    if(!type.toHtml)
         return PB.onError('Invalid content type: object is missing toHtml method', name)
-    
+
     PB.M.Forum.contentTypes[name] = type
 }
 
@@ -377,7 +377,7 @@ PB.M.Forum.addContentType('text', {
 PB.M.Forum.addContentType('bbcode', {
     toHtml: function(content) {
         var bbcodeParse = XBBCODE.process({ text: content });
-        var parsedText  = bbcodeParse.html.replace(/\n/g, '<br />'); 
+        var parsedText  = bbcodeParse.html.replace(/\n/g, '<br />');
         return parsedText;
     }
 })
@@ -418,12 +418,12 @@ PB.M.Forum.addContentType('profile', {
         else
             return '<img class="imgInBox" src=' + content + ' />';
         /*var keysNotShow = ['content', 'type'];
-        for (var key in puff.payload) {
-            var value = puff.payload[key];
-            if (keysNotShow.indexOf(key)==-1 && value && value.length) {
-                toRet += '<div><span class="profileKey">' + key + ': </span><span class="profileValue">' + value + '</span></div>';
-            }
-        }*/
+         for (var key in puff.payload) {
+         var value = puff.payload[key];
+         if (keysNotShow.indexOf(key)==-1 && value && value.length) {
+         toRet += '<div><span class="profileKey">' + key + ': </span><span class="profileValue">' + value + '</span></div>';
+         }
+         }*/
     }
 })
 
@@ -431,18 +431,18 @@ PB.M.Forum.addContentType('file', {
     toHtml: function(content, puff) {
         return (
             puff.payload.filename
-            )
+        )
     }
 
 })
 
 // TODO: Add support for LaTex
 /*PB.M.Forum.addContentType('LaTex', {
-    toHtml: function(content) {
-        var safe_content = XBBCODE.process({ text: content }) 
-        return '<p>' + safe_content.html + '</p>'
-    }
-}) */
+ toHtml: function(content) {
+ var safe_content = XBBCODE.process({ text: content })
+ return '<p>' + safe_content.html + '</p>'
+ }
+ }) */
 
 
 // Flag a puff
@@ -452,32 +452,33 @@ PB.M.Forum.flagPuff = function (sig) {
     var routes = [];
     var type = 'flagPuff';
     var content = sig;
-    
+    var puff; // variable for leaking the signed puff out of the secure zone
+
     payload.time = Date.now();
 
-    PB.useSecureInfo(function(identities, currentUsername, privateRootKey, privateAdminKey, privateDefaultKey) {    
+    PB.useSecureInfo(function(identities, currentUsername, privateRootKey, privateAdminKey, privateDefaultKey) {
 
         if(!currentUsername) {
             alert("You must first set your username before you can flag content");
             return false;
         }
         /*if(!currentUsername == PB.getPuffBySig(sig).username) {
-            alert("You must set your identity to the author of the puff you want to flag");
-        }*/
+         alert("You must set your identity to the author of the puff you want to flag");
+         }*/
         if(!privateAdminKey) {
             alert("You must first set your private admin key before you can flag content");
             return false;
         }
-    
-        var puff = PB.buildPuff(currentUsername, privateAdminKey, routes, type, content, payload);
+
+        puff = PB.buildPuff(currentUsername, privateAdminKey, routes, type, content, payload);
     })
 
     var data = { type: 'flagPuff'
-               , puff: puff
-               };
+        , puff: puff
+    };
 
     var prom = PB.Net.PBpost(PB.CONFIG.puffApi, data);
-    
+
     prom = prom.then(function(result){
         // var storedShells = PB.Persist.get('shells');
         // var filteredShells = storedShells.filter(function(s){return s.sig != content && s.content != content});
@@ -500,7 +501,7 @@ PB.M.Forum.metaFields = []
 PB.M.Forum.context = {};
 PB.M.Forum.addMetaFields = function(fieldInfo, context, excludeContext) {
     // NOTE: this isn't used outside of publishEmbed.js, but it might provide a good basis for generic/required metadata
-    
+
     if (!fieldInfo.name) return console.log('Invalid meta field name.');
 
     // supported type: text, textarea, pulldown, array
@@ -536,29 +537,29 @@ PB.M.Forum.addMetaFields = function(fieldInfo, context, excludeContext) {
 
 PB.M.Forum.addMetaFields(
     {name: 'reply privacy',
-     type: 'pulldown',
-     enum: ['', 'public', 'private', 'anonymous', 'invisible'],
-     defaultValue: ''});
+        type: 'pulldown',
+        enum: ['', 'public', 'private', 'anonymous', 'invisible'],
+        defaultValue: ''});
 
 PB.M.Forum.addMetaFields(
     {name: 'content license',
-     type: 'pulldown',
-     enum: ['', 'CreativeCommonsAttribution', 'GNUPublicLicense', 'Publicdomain', 'Rights-managed', 'Royalty-free'],
-     defaultValue: ''});
+        type: 'pulldown',
+        enum: ['', 'CreativeCommonsAttribution', 'GNUPublicLicense', 'Publicdomain', 'Rights-managed', 'Royalty-free'],
+        defaultValue: ''});
 
 PB.M.Forum.addMetaFields(
     {name: 'tags',
-     type: 'array',
-     validator: function(v){return /^[a-z0-9]+$/i.test(v)}
-     },
+        type: 'array',
+        validator: function(v){return /^[a-z0-9]+$/i.test(v)}
+    },
     false, 'profile');
 
 PB.M.Forum.addMetaFields(
     {name: 'language',
-     type: 'text',
-     defaultValue: function(){return puffworldprops.view.language}});
+        type: 'text',
+        defaultValue: function(){return puffworldprops.view.language}});
 
 PB.M.Forum.addMetaFields(
     {name: 'name',
-     type: 'text'},
+        type: 'text'},
     'profile');

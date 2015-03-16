909dffdbc3f8fc33de62e7dee096f38f31411f2c
Marcus Hitchins <marcus@hhost.me>
2015-03-16 11:34:44 +0000

    Updated UI for easy friend removal

--- Examples/Songr-Tutorial/js/songr.js
+++ Examples/Songr-Tutorial/js/songr.js
@@ -25,7 +25,7 @@ function addFriend() {
             EB.setPreference("friends",prefs.friends);
         }
 
-        $("#friendsList").append("<li>" + friendToAdd + "</li>");
+        $("#friendsList").append("<li><button onclick='removeFriend(\""+ friendToAdd+"\")' class=\"btn btn-primary\" title='Remove " + friendToAdd + "'>x</button>" + friendToAdd + "</li>");
     })
     .catch (function(err) {
         alert(err)
@@ -79,7 +79,7 @@ function getPrefs() {
 function showFriends() {
     var friends = getFriends();
     friends.forEach(function(friend) {
-        $("#friendsList").append("<li>" + friend + "</li>");
+        $("#friendsList").append("<li><button onclick='removeFriend(\""+ friend +"\")' class=\"btn btn-primary\" title='Remove " + friend + "'>x</button>" + friend + "</li>");
     });
 }
 
